ad1b5b410fa177b2471861d06ed38c41c2e45920
Oscar Gonzalez <racsogonzalez30@gmail.com>
2022-08-22 14:14:36 -0600

    navbar - color, scroll and html structure

--- web/static/js/navbar.js
+++ web/static/js/navbar.js
@@ -8,16 +8,33 @@ function open_menu() {
 function close_menu() {    
     document.getElementById("sidebar").style.visibility = "hidden";
     document.getElementById("sidebar").style.width = "0";
-    document.getElementsByClassName("main-panel")[0].style.marginLeft = "0";
+    document.getElementsByClassName("main-panel")[0].style.marginLeft = "15px";
     document.getElementsByClassName("main-panel")[0].style.padding = "15px 15px 15px 15px";
   }
 
 function menu() {
     //alert("Pressed");
-if (document.getElementById("checkbox").checked) {
-    open_menu();
+    if (document.getElementById("checkbox").checked) {
+        open_menu();
+    }
+    else {
+        close_menu();
+    }
 }
-else {
-    close_menu();
-}
+
+
+navbar = document.getElementById("nav");
+main_panel = document.getElementsByClassName("main-panel")[0];
+prev_position = main_panel.scrollTop;
+function navbar_visibility() {
+    console.log(prev_position);
+    console.log(main_panel.scrollTop);
+    if (main_panel.scrollTop > prev_position) {
+        navbar.style.top = "-50px";
+    }
+    else{
+        navbar.style.top = "inherit";
+    }
+    prev_position = main_panel.scrollTop;
+    //getSelection("navbar").style.visibility("hidden");
 }
